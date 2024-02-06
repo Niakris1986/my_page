c947385d61fde5d83a1e89545a68e9cec99cfdde
Nastya <ivannikova@msk.avnt.ru>
2024-02-07 01:43:03 +0300

    test_zodiacs, test_zodiacs_redirect added

--- horoscope/tests.py
+++ horoscope/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from horoscope.views import zodiac_dict
+
 
 class TestHoroscope(TestCase):
 
@@ -19,3 +21,20 @@ class TestHoroscope(TestCase):
         response = self.client.get('/horoscope/7')
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, '/horoscope/libra')
+
+    def test_zodiacs(self):
+        zodiacs = zodiac_dict
+        for el in zodiacs:
+            response = self.client.get(f'/horoscope/{el}')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(
+                zodiac_dict[el],
+                response.content.decode()
+            )
+
+    def test_zodiacs_redirect(self):
+        list_of_zodiacs = list(zodiac_dict)
+        for i, v in enumerate(list_of_zodiacs, start=1):
+            response = self.client.get(f'/horoscope/{i}')
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(response.url, f'/horoscope/{v}')
